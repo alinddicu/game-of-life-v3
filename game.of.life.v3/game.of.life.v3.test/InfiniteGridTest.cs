87e01cc77225a1dd44b0b8be707f9c39b572b056
alinddicu <alinddicu@yahoo.com>
2016-04-29 15:13:18 +0200

    more tests

--- game.of.life.v3/game.of.life.v3.test/InfiniteGridTest.cs
+++ game.of.life.v3/game.of.life.v3.test/InfiniteGridTest.cs
@@ -1,6 +1,5 @@
 ﻿namespace game.of.life.v3.test
 {
-    using System.Linq;
     using NFluent;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -25,6 +24,24 @@
                 new Cell(-1, 1),
                 new Cell(0, 1),
                 new Cell(1, 1));
+        }
+
+        [TestMethod]
+        public void GivenGridWith1CellWhenGetCellOnThatCellThenReturnTheThatCell()
+        {
+            var cell = new Cell(0, 0, CellState.Alive);
+            var grid = new InfiniteGrid();
+            grid.Add(cell);
+
+            Check.That(grid.Get(0, 0).State).IsEqualTo(CellState.Alive);
+        }
+
+        [TestMethod]
+        public void GivenEmptyGridWhenGetAnyCellThenReturnDeadCell()
+        {
+            var grid = new InfiniteGrid();
+
+            Check.That(grid.Get(0, 0).State).IsEqualTo(default(CellState));
         }
     }
 }
