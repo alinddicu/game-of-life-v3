c5f018f86c6b7799d191a8b76b25a6e62c223343
alinddicu <alinddicu@yahoo.com>
2016-05-04 11:56:07 +0200

    extracted GoLRunner

--- game.of.life.v3/game.of.life.v3.desktop/GoLForm.cs
+++ game.of.life.v3/game.of.life.v3.desktop/GoLForm.cs
@@ -1,80 +1,33 @@
 ﻿namespace game.of.life.v3.desktop
 {
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Windows.Forms;
 
     public partial class GoLForm : Form
     {
         private const int ButtonWidth = 60;
-        private const int ButtonGap = ButtonWidth + 1;
 
-        private readonly List<CellButton> _buttons = new List<CellButton>();
-        private readonly IGrid _grid = new RectangularInfinite2DGrid();
-        private Cycle _cycle;
+        private readonly GoLRunner _goLRunner;
 
         public GoLForm()
         {
             InitializeComponent();
             StartPosition = FormStartPosition.CenterScreen;
+            _goLRunner = new GoLRunner(_cellsPanel, ButtonWidth);
         }
 
         private void GoLForm_Load(object sender, System.EventArgs e)
         {
-            InitCellButtons();
-        }
-
-        private void InitCellButtons()
-        {
-            _buttons.Clear();
-            var panelWidth = _cellsPanel.Width;
-            var panelHeight = _cellsPanel.Height;
-
-            var horizontalButtonsCount = panelWidth / ButtonGap;
-            var verticalButtonsCount = panelHeight / ButtonGap;
-            for (var hCounter = 0; hCounter < horizontalButtonsCount; hCounter++)
-            {
-                for (var vCounter = 0; vCounter < verticalButtonsCount; vCounter++)
-                {
-                    var cellButton = CreateCellButton(vCounter, hCounter);
-                    _buttons.Add(cellButton);
-                }
-            }
-
-            _cellsPanel.Controls.Clear();
-            _cellsPanel.Controls.AddRange(_buttons.ToArray());
-        }
-
-        private static CellButton CreateCellButton(int vCounter, int hCounter)
-        {
-            return new CellButton(vCounter, hCounter, ButtonGap * vCounter, ButtonGap * hCounter)
-            {
-                Width = ButtonWidth,
-                Height = ButtonWidth,
-                Text = $"({vCounter},{hCounter})",
-                BackColor = CellButton.BackColorDefault
-            };
+            _goLRunner.InitCellButtons();
         }
 
         private void cycleButton_Click(object sender, System.EventArgs e)
         {
-            if (_cycle == null)
-            {
-                _grid.AddCells(_buttons.Select(b => b.Cell).Where(c => c.IsAlive).ToArray());
-                _cycle = new Cycle(_grid);
-                _cellsPanel.Enabled = false;
-            }
-
-            _cycle.Run();
-            _buttons.ForEach(b => b.RefreshCell(_grid));
+            _goLRunner.Cycle();
         }
 
         private void resetButton_Click(object sender, System.EventArgs e)
         {
-            InitCellButtons();
-            _grid.Reset();
-            _cellsPanel.Enabled = true;
-            _cycle = null;
+            _goLRunner.Reset();
         }
     }
 }
---
+++ game.of.life.v3/game.of.life.v3.desktop/GoLRunner.cs
@@ -0,0 +1,76 @@
+﻿namespace game.of.life.v3.desktop
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows.Forms;
+
+    public class GoLRunner
+    {
+        private readonly Panel _cellsPanel;
+        private readonly int _buttonWidth;
+        private readonly List<CellButton> _buttons = new List<CellButton>();
+        private readonly IGrid _grid = new RectangularInfinite2DGrid();
+        private Cycle _cycle;
+
+        public GoLRunner(Panel cellsPanel, int buttonWidth)
+        {
+            _cellsPanel = cellsPanel;
+            _buttonWidth = buttonWidth;
+        }
+
+        private int ButtonGap { get { return _buttonWidth + 1; } }
+
+        public void InitCellButtons()
+        {
+            _buttons.Clear();
+            var panelWidth = _cellsPanel.Width;
+            var panelHeight = _cellsPanel.Height;
+
+            var horizontalButtonsCount = panelWidth / ButtonGap;
+            var verticalButtonsCount = panelHeight / ButtonGap;
+            for (var hCounter = 0; hCounter < horizontalButtonsCount; hCounter++)
+            {
+                for (var vCounter = 0; vCounter < verticalButtonsCount; vCounter++)
+                {
+                    var cellButton = CreateCellButton(vCounter, hCounter);
+                    _buttons.Add(cellButton);
+                }
+            }
+
+            _cellsPanel.Controls.Clear();
+            _cellsPanel.Controls.AddRange(_buttons.ToArray());
+        }
+
+        private CellButton CreateCellButton(int vCounter, int hCounter)
+        {
+            return new CellButton(vCounter, hCounter, ButtonGap * vCounter, ButtonGap * hCounter)
+            {
+                Width = _buttonWidth,
+                Height = _buttonWidth,
+                Text = $"({vCounter},{hCounter})",
+                BackColor = CellButton.BackColorDefault
+            };
+        }
+
+        public void Cycle()
+        {
+            if (_cycle == null)
+            {
+                _grid.AddCells(_buttons.Select(b => b.Cell).Where(c => c.IsAlive).ToArray());
+                _cycle = new Cycle(_grid);
+                _cellsPanel.Enabled = false;
+            }
+
+            _cycle.Run();
+            _buttons.ForEach(b => b.RefreshCell(_grid));
+        }
+
+        public void Reset()
+        {
+            InitCellButtons();
+            _grid.Reset();
+            _cellsPanel.Enabled = true;
+            _cycle = null;
+        }
+    }
+}
