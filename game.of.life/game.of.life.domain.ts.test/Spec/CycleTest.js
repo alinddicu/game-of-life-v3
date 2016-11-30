dd3a8c6e3af7a38c9936c64381fc40424a97fc68
Alin Dicu <alinddicu@yahoo.com>
2016-11-30 13:52:04 +0100

    init Cycle

---
+++ game.of.life/game.of.life.domain.ts.test/Spec/CycleTest.js
@@ -0,0 +1,16 @@
+/// <reference path= "../../game.of.life.domain.ts/Cell.ts" />
+/// <reference path= "../../game.of.life.domain.ts/CellState.ts" />
+/// <reference path= "../../game.of.life.domain.ts/RectangularInfinite2DGrid.ts" />
+/// <reference path= "../../game.of.life.domain.ts/Cycle.ts" />
+/// <reference path= "../Scripts/typings/jasmine/jasmine.d.ts" />
+describe("Cycle", function () {
+    it("GivenSimpleMutationCompletionWith1CellWhenDiscoverCleanRevivalThenGridHas16Cells", function () {
+        var initialCells = [new Cell(0, 0, CellState.Alive), new Cell(1, 0, CellState.Alive), new Cell(0, 1, CellState.Alive)];
+        var grid = new RectangularInfinite2DGrid(initialCells);
+        var cycle = new Cycle();
+        var cells = cycle.run(grid).Cells;
+        //Check.That(cells.Where(c => c.IsAlive())).IsOnlyMadeOf(initialCells.Union(new [] { new Cell(1, 1) }));
+        expect(cells.length).toBe(16);
+    });
+});
+//# sourceMappingURL=CycleTest.js.map
---
+++ game.of.life/game.of.life.domain.ts/Cycle.ts
@@ -0,0 +1,25 @@
+﻿class Cycle {
+
+    run(currentGrid : RectangularInfinite2DGrid): RectangularInfinite2DGrid {
+        var newGrid = this.initializeNewGrid(currentGrid);
+        this.computeAndCompleteMutation(newGrid);
+        this.prepareForNextCycle(newGrid);
+        return newGrid;
+    }
+
+    private initializeNewGrid(currentGrid : RectangularInfinite2DGrid): RectangularInfinite2DGrid {
+        var newGrid = new RectangularInfinite2DGrid(currentGrid.Cells.ToList<Cell>().Select(c => new Cell(c.x, c.y, c.state)).ToArray());
+        newGrid.discover();
+        return newGrid;
+    }
+
+    private computeAndCompleteMutation(newGrid : RectangularInfinite2DGrid): void {
+        newGrid.Cells.ToList<Cell>().ForEach(cell => cell.computeNextMutation(newGrid.getNeighbours(cell).ToList<Cell>().Count(c => c.isAlive())));
+        newGrid.Cells.ToList<Cell>().ForEach(cell => cell.completeMutation());
+    }
+
+    private prepareForNextCycle(newGrid : RectangularInfinite2DGrid): void {
+        newGrid.discover();
+        newGrid.clean();
+    }
+}
