554c9bcec2b9448f80337a73834290a9ee0b0650
Alin Dicu <alinddicu@yahoo.com>
2019-01-17 22:29:22 +0100

    implemented NextCycle() yay!!!!!!!!!!!!!!
    it works!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- game.of.life/game.of.life.ts.web/scripts/GoL/Drawing/Board.ts
+++ game.of.life/game.of.life.ts.web/scripts/GoL/Drawing/Board.ts
@@ -1,17 +1,23 @@
 ﻿/// <reference path="../../../typings/knockout.d.ts"/>
+/// <reference path="../../../typings/linq.d.ts"/>
 
 'use strict';
 
 namespace GoL.Drawing {
+	import Cycle = Logic.Cycle;
+	import RectangularInfinite2DGrid = Logic.RectangularInfinite2DGrid;
+
 	export class Board {
 
+		private Cycle: Cycle = new Cycle();
+		private _gridHistory: RectangularInfinite2DGrid[] = [];
 		public BoardLines: BoardLine[] = [];
 
 		constructor(goLOptions: IGoLOptions) {
 			this.InitCellButtonsInSquare(goLOptions);
 		}
 
-		private InitCellButtonsInSquare(goLOptions: IGoLOptions): void  {
+		private InitCellButtonsInSquare(goLOptions: IGoLOptions): void {
 
 			const numberOfCellsPerRow = goLOptions.NumberOfCellsPerRow;
 			for (let hCounter = 0; hCounter < numberOfCellsPerRow; hCounter++) {
@@ -32,6 +38,31 @@ namespace GoL.Drawing {
 
 		private static GetCellText(vCounter: number, hCounter: number, isShowCellsCoordinates: boolean): string {
 			return isShowCellsCoordinates ? `(${vCounter},${hCounter})` : "";
+		}
+
+		public NextCycle(): void {
+			if (this._gridHistory.length === 0) {
+				this._gridHistory.push(this.GetCurrentGrid());
+				//_cellsPanel.Enabled = false;
+			}
+
+			this._gridHistory.push(this.Cycle.Run(this.GetLastGrid()));
+			this.RefreshCellButtons();
+		}
+
+		private GetCurrentGrid(): RectangularInfinite2DGrid {
+			let cells: Cell[] = Enumerable.from(this.BoardLines)
+				.selectMany((bl: BoardLine) => bl.ButtonCells).where((cb: CellButton) => cb.Cell.IsAlive())
+				.select(x => x.Cell).toArray();
+			return new RectangularInfinite2DGrid(cells);
+		}
+
+		private RefreshCellButtons(): void {
+			Enumerable.from(this.BoardLines).forEach((b: BoardLine) => b.RefreshCells(this.GetLastGrid()));
+		}
+
+		private GetLastGrid(): RectangularInfinite2DGrid {
+			return this._gridHistory[this._gridHistory.length - 1];
 		}
 	}
 }
--- game.of.life/game.of.life.ts.web/scripts/GoL/Drawing/BoardLine.ts
+++ game.of.life/game.of.life.ts.web/scripts/GoL/Drawing/BoardLine.ts
@@ -1,13 +1,20 @@
 ﻿/// <reference path="../../../typings/knockout.d.ts"/>
+/// <reference path="../../../typings/linq.d.ts"/>
 
 'use strict';
 
 namespace GoL.Drawing {
+	import RectangularInfinite2DGrid = Logic.RectangularInfinite2DGrid;
+
 	export class BoardLine {
 		public ButtonCells: CellButton[];
 
 		constructor(buttonCells: CellButton[]) {
 			this.ButtonCells = buttonCells;
+		}
+
+		public RefreshCells(grid: RectangularInfinite2DGrid): void {
+			Enumerable.from(this.ButtonCells).forEach((cb: CellButton) => cb.RefreshCell(grid));
 		}
 	}
 }
