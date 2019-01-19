5b0cd5b7383f1d28c61a41714d27026636715207
Alin Dicu <alinddicu@yahoo.com>
2019-01-19 14:09:52 +0100

    kind of better color styling

--- game.of.life/game.of.life.ts.web/scripts/GoL/Drawing/ParamsForm.ts
+++ game.of.life/game.of.life.ts.web/scripts/GoL/Drawing/ParamsForm.ts
@@ -9,8 +9,8 @@
 				cellSize: this.valueOrDefault<number>("cellSize", 50),
 				normalMutationDelay: this.valueOrDefault<number>("normalMutationDelay", 100),
 				rapidMutationDelay: this.valueOrDefault<number>("rapidMutationDelay", 10),
-				aliveCellColor: this.valueOrDefault<string>("aliveCellColor", "#2e9cca"),
-				deadCellColor: this.valueOrDefault<string>("deadCellColor", "aaabb8")
+				aliveCellColor: this.valueOrDefault<string>("aliveCellColor", "#374785"),
+				deadCellColor: this.valueOrDefault<string>("deadCellColor", "#a8d0e6")
 			};
 
 			return new Board(this.options);
