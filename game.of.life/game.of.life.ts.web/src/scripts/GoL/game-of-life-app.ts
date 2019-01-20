02f449b79f98bb642d8736c2a9fd6628bee8a163
Alin Dicu <alinddicu@yahoo.com>
2019-01-20 17:53:46 +0100

    separated scripts from html

--- game.of.life/game.of.life.ts.web/src/scripts/GoL/game-of-life-app.ts
+++ game.of.life/game.of.life.ts.web/src/scripts/GoL/game-of-life-app.ts
@@ -1,16 +1,52 @@
 ﻿
 'use strict';
 
-//let params: GoL.Drawing.ParamsForm = new GoL.Drawing.ParamsForm();
-//let board: GoL.Drawing.Board = params.init();
+var params = new GoL.Drawing.ParamsForm();
+var board = params.init();
 
-//let viewModel = {
-//	board: ko.observable(board)
-//}
+var viewModel = {
+	board: ko.observable(board),
+	isParamsVisible: ko.observable(false),
+	isExportVisible: ko.observable(false),
+	isImportVisible: ko.observable(false)
+};
+ko.applyBindings(viewModel);
 
-//function init(): void {
-//	params = new GoL.Drawing.ParamsForm();
-//	viewModel.board(params.init());
-//}
+function init() {
+	board = params.init();
+	board.initCellButtonsInSquare(params.options);
+	viewModel.board(board);
+	viewModel.isParamsVisible(false);
+}
 
-//ko.applyBindings(viewModel);
+function showParams() {
+	var currentValue = viewModel.isParamsVisible();
+	viewModel.isParamsVisible(!currentValue);
+	viewModel.board().isEnabled(currentValue);
+}
+
+function exportAliveCells() {
+	var currentValue = viewModel.isExportVisible();
+	viewModel.board().isEnabled(currentValue);
+	viewModel.isExportVisible(!currentValue);
+	var textArea = document.getElementById("exportTextArea") as HTMLTextAreaElement;
+	if (textArea) {
+		textArea.value = board.export();
+	}
+}
+
+function showImportAliveCells() {
+	var currentValue = viewModel.isImportVisible();
+	viewModel.isImportVisible(!currentValue);
+	viewModel.board().isEnabled(currentValue);
+}
+
+function importAliveCells() {
+	var textArea = document.getElementById("importTextArea") as HTMLTextAreaElement;
+	if (textArea && textArea.value) {
+		board.import(textArea.value);
+	}
+
+	viewModel.isImportVisible(false);
+	viewModel.board().isEnabled(true);
+}
